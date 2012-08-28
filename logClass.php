95d8243f4dc1c80ca6895cdc020426652aa17eba
fitnr <fitnr@fakeisthenewreal>
2012-08-28 13:49:28 -0400

    In process of making logging better, non static

--- logClass.php
+++ logClass.php
@@ -1,90 +1,163 @@
 <?php
-/*
-* static class of logging functions
-* these should work for any twitter bot or cron job
+
+/**
+  * class of logging functions
+  * 
+  * This loggering class should work for any twitter bot or cron job.
+  * Strings are saved and output once to a file. The email_log function can be
+  * set to email the log once a week on a specific day and time.
+  * Some ideas and code borrowed from https://github.com/katzgrau/KLogger/.
 */
 class logger
 {
-  # holds the text
-  static public $output;
-  # higher is more verbose
-  static public $log_level = 10;
-  # destination for these wonderful tidbits
-  static public $log_file;
-  static public $vocal = False;
-  static public $log_sender;
-  static private $log_recipient = "fake is the new real <postmaster@fakeisthenewreal.org>";
-  static public $msec = FALSE;
-  static private $prefix = '';
 
-  public static function log($str, $level=2) {
-    # compare passed log level to that defined in the class
-    if ($level <= self::$log_level):
+  /**
+   * Error severity, from low to high. From BSD syslog RFC, secion 4.1.1,
+   * Except for OFF, which is custom.
+   * @link http://www.faqs.org/rfcs/rfc3164.html
+  */
+  const OFF    = -1;// Log nothing at all
+  const EMERG  = 0; // Emergency: system is unusable
+  const ALERT  = 1; // Alert: action must be taken immediately
+  const CRIT   = 2; // Critical: critical conditions
+  const ERR    = 3; // Error: error conditions
+  const WARN   = 4; // Warning: warning conditions
+  const NOTICE = 5; // Notice: normal but significant condition
+  const INFO   = 6; // Informational: informational messages
+  const DEBUG  = 7; // Debug: debug messages
+  const ALL    = 10; // All error messages
+
+  // Holds the current threshold for logging
+  public $_log_level = self::INFO;
+  
+  // destination for these wonderful tidbits
+  public $_log_file;
+
+  // True will print messages to STDOUT
+  public $vocal = False; 
+  
+  public $log_sender;
+  private $log_recipient = '';
+  
+  private $_prefix = '';
+
+  // A few helpful shortcuts for formatting the time
+  const FORMAT_YMD_SEC = 'y-m-d H:i:s';
+  const FORMAT_YMD_MSEC = 'y-m-d H:i:s:u';
+  private $_format = FORMAT_YMD_SEC;
+
+  private static $_default_perms = 0777;
+
+  // The internal status, one of three constants.
+  private $_status = '';
+  const _OPEN_FAILED = 1;
+  const _LOG_OPEN = 2;
+  const _OPEN_FAILED = 3;
+
+  private $_messages = array(
+      'writefail'   => 'The file could not be written to. Check that appropriate permissions have been set.',
+      'opensuccess' => 'The log file was opened successfully.',
+      'openfail'    => 'The file could not be opened. Check permissions.',
+  );
+
+  function __construct($log_file, $log_level) {
+    $this->_log_file = $log_file;
+    $this->_log_level = $log_level;
+
+    // Create the directory if none exists.
+    $logdir = dirname($log_file);
+    if (!file_exists($logdir)) {
+      mkdir($logdir, self::$_default_perms, true);
+    }
+
+    if (file_exists($this->_log_file) && !is_writable($this->_log_file)) {
+        $this->_status = self::_OPEN_FAILED;
+        echo $this->_messages['writefail'];
+        return;
+    }
+
+    if (($this->_fileHandle = fopen($this->_log_file, 'a'))) {
+        $this->_status = self::_LOG_OPEN;
+        $this->log($this->_messages['opensuccess'], self::INFO);
+
+    } else {
+        $this->_status = self::_OPEN_FAILED;
+        echo $this->_messages['openfail'];
+    }
+  }
+
+  /**
+   * Save a single string to the log file
+  */
+  public function log($str, $level=self::INFO) {
+    // compare passed log level to that defined in the class
+    if ($this->_status == self::_LOG_OPEN && $level <= $this->$_log_level):
       try {
-        $string = self::encode_for_log(self::$msec, $str);
-        if (self::$vocal == True):
+        $string = $this->encode_for_log($str, $this->_format);
+        
+        if ($this->vocal == True):
           echo $string;
         endif;
-        self::$output .= $string;
+
+        $this->write_line($string);
+
       } catch (Exception $e) {
-        echo $e->getMessage() . "\n" . $str;
+        echo $e->getMessage() . PHP_EOL . $str;
       }
     endif;
   }
 
-  private static function encode_for_log($microsec, $str) {
-    $str = self::prefix . $str;
-    if (self::$msec == TRUE):
-      $m = microtime(true);
-      $m = substr(round($m - floor($m), 4), 2);
-      return sprintf("%s%0-4s\t%s", date("\ny-m-d H:i:s:"), $m, $str);
-    else:
-      return date("\ny-m-d H:i:s\t") . $str;
-    endif;
+  private function encode_for_log($str, $timeformat) {
+    $str = $this->_prefix . $str;
+    $d = new DateTime();
+
+    return sprintf('%s\t%s' . PHP_EOL, $d->format($timeformat), $str);
   }
 
-  public static function set_prefix($prefix) {
-    self::$prefix = $prefix;
+  /**
+   * Writes a line to the log.
+   *
+   * @param string $line Line to write to the log
+   * @return void
+   */
+  private function _write_line ($line) {
+    if (fwrite($this->_fileHandle, $line) === false)
+      echo $this->_messages['writefail'];
   }
 
-  // need non-empty output and a proper file opening
-  public static function save_log($str='') {
+  /**
+   * Sets a prefix for the message part of a line
+   *
+   * @param string $prefix The string to precede each message
+  */
+  public function set_prefix($prefix) {
+    $this->_prefix = $prefix;
+  }
 
-    $str = ($str) ? self::encode_for_log(self::$msec, $str): '' ;
+  public function set_time_format ($format) {
+    $this->_format = $format;
+  }
 
-    if (self::$log_file == ""):
-      return -1;
-    elseif ($fh = fopen(self::$log_file, "a")):
-      fwrite($fh, self::$output . $str);
-      fclose($fh);
-      self::$output = "";
-      return true;
-    else:
-      error_log("\nError saving log: " . self::report_err() . self::$output, 3, self::$log_file);
-      return -2;
-    endif;
- }
-
-  # $e should be array generated by error_get_last()
-  private static function report_err($e = NULL) {
+  // $e should be array generated by error_get_last()
+  private function report_err($e = NULL) {
     if ($e === NULL)
       $e = error_get_last();
     return sprintf("Type %s on line %s of %s: %s",  $e['type'],  $e['line'],$e['file'],$e['message']);
   }
 
-  private static function get_logfile() {
+  private function get_logfile() {
     return file_get_contents(self::$log_file);
   }
 
-  #delete the log
-  private static function truncate_log() {
+  // Delete the log
+  private function truncate_log() {
     $f = fopen(self::$log_file,'w');
     if ($f === false)
       throw new Exception("error truncating log file");
   }
 
-  #send off the log
-  private static function emailer($log) {
+  // Send off the log.
+  private function emailer($log) {
     $headers  = "From: " . self::$log_sender . "\r\n";
     $headers .= "X-Mailer:PHP " . phpversion() . "\r\n";
     $headers .= "Content-type:text/plain; charset=UTF-8";
@@ -96,14 +169,14 @@ class logger
     endif;
   }
 
-  /*
-  * args:
-  *    day: day of week e.g. Monday
-  *   hour: hour on 24 hour scale, e.g. 13 for 1 pm, 0 for midnight
-  * output:
-  *   email sent to
+  /**
+   * Sends an email if the program is run on a given date and day.
+   *
+   * @param string $day The day of week e.g. Monday
+   * @param string $hour The hour on 24 hour scale, e.g. 13 for 1 pm, 0 for midnight
+   * @return void
   */
-  public static function email_log($day, $hour) {
+  public function email_log($day, $hour) {
     if ( date('l') == $day && date('G') == $hour ):
       try {
         $log = self::get_logfile();
@@ -111,7 +184,7 @@ class logger
         self::$output = "";
         self::truncate_log();
       } catch(Exception $e) {
-        # useful if server is set to email any result.
+        // Useful if server is set to email any result.
         echo self::$output;
         echo $e->getMessage();
       }
